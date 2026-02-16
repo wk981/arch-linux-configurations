bd031176e63f7b65b1122aa157c9accb90644deb
wk981 <jamesim445@gmail.com>
2026-02-16 12:23:06 +0800

    Revamped readme and added new dot configs.

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # arch-linux-configurations
+
+## Bootstrap
+
+### Command
+
+```bash
+
+sh arch-linux-essential.sh
+```
+
+### What does it install
+
+1. The Core Desktop (Hyprland Suite)
+
+    Hyprland & Wayland,
+XDG-Desktop-Portal-Hyprland, QT5/6-Wayland
+
+2. User Interface & Visuals
+
+    Waybar, Rofi, Hyprpaper, SwayNC, SDDM
+
+3. Productivity & Utilities
+
+    Kitty, Grim & Slurp, Cliphist, Fastfetch
+
+4. System & Networking
+
+    UFW, Docker, NM-Connection-Editor, Pamixer, Waylock
+
+
+## Configs
+
+### Software
+
+1. Fastfetch
+2. hypr
+3. kitty
+4. rofi
+5. waybar
+
+### Installation
+
+Command
+
+```bash
+
+
+```
--- arch-linux-essential.sh
+++ arch-linux-essential.sh
@@ -1,21 +1,56 @@
-sudo pacman -Sy hyprland \
+echo "----------------------------"
+echo "Installing Packages Now"
+echo "----------------------------"
+
+sudo pacman -Sy wayland \
+                qt6-wayland \
+                qt5-wayland \
+                xdg-desktop-portal-hyprland \
+                hyprland \
                 kitty \
+                dolphin \
+                rofi \
                 waybar \
+                waylock \
                 hyprpaper \
                 swaync \
                 grim \
                 slurp \
                 cliphist \
-                rofi \
                 fastfetch \
-                wayland \ 
-                qt6-wayland \ 
-                qt5-wayland \
-                xdg-desktop-portal-hyprland \
-                nvim \
                 sddm \
+                pamixer \
+                nm-connection-editor \
+		docker \
+                ufw
 
-sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+# Install yay and some necessay yay
+# cd /tmp
+
+# sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+# yay -S neohtop
+
+# Enable some packages
+echo "----------------------------"
+echo "Enabling Packages Now"
+echo "----------------------------"
+sudo systemctl enable docker
+sudo systemctl enable sddm
+sudo systemctl enable ufw
+
+# Docker post installation
+echo "----------------------------"
+echo "Post Docker Installation"
+echo "----------------------------"
+sudo usermod -aG docker $USER
+
+# Ohmybash
+echo "----------------------------"
+echo "Installing OhMyBash Now"
+echo "----------------------------"
+sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" && exit
 sudo sed -i 's/^OSH_THEME=.*/OSH_THEME="modern"/' ~/.bashrc
 
-sudo systemctl enable sddm
+echo "----------------------------"
+echo "Set up successfully!!"
+echo "----------------------------"
